a827cc7a836bb146951e6db23456a8eeb2fa3423
jorozcop <maechavarriaor@gmail.com>
2023-09-13 11:04:33 -0500

    ajustes
    ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -103,6 +103,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-file-pdf',
+        titulo: 'Descargar PDF',
+        download: 'downloads/331502_CF25_DU.pdf',
+      },
+      {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
